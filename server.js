6b8c56ad8f57d27f2129b227f0fe812dd7bdc693
Ramanrm <ramanathangcemech@gmail.com>
2016-09-23 08:03:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,53 +4,70 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne = {
-    title: 'article-one | Ram',
-    date: '23 sep, 2016',
-    heading: 'Article One',
-    content: `
-    <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
-    </p>
-    <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
-    </p>
-    <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
-    </p>`
-    
-    
+var articles = {
+    var articleOne: {
+        title: 'article-one | Ram',
+        date: '23 sep, 2016',
+        heading: 'Article One',
+        content: `
+        <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
+        </p>
+        <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
+        </p>
+        <p> the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
+        </p>`
+        },
+    var articleTwo: {
+        title: 'article-two | Ram',
+        date: '19 sep, 2016',
+        heading: 'Article Two',
+        content: `
+        <p> Article two's content page is here. All its content will be displayed in this area.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
+        </p>
+        `},
+    var articleThree : {
+        title: 'article-Three | Ram',
+        date: '22 sep, 2016',
+        heading: 'Article Three',
+        content: `
+        <p> Article three's content will be displayed here.  the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.the content will be displayed here.
+        </p>
+        `}
 };
+
 var createTemplate = function(data) {
     var date = data.date;
     var content = data.content;
     var heading = data.heading;
     var title = data.title;
    
-        var htmlTemplate = `
-        <html>
+    var htmlTemplate = `
+    <html>
         <head>
-        <title>${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
+            <title>
+                    ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
             <div class ="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <div>
-                ${date}
-            </div>
-            <div>
-            <h1> ${heading}</h1>
-            <h2> You just reached article one</h2>
-            ${content}
-            </p>
-            </div>
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    <h1> ${heading}</h1>
+                    <h2> You just reached article one</h2>
+                    ${content}
+                </div>
             </div>
         </body>
-        </html>
-        `;
+    </html>
+    `;
     return htmlTemplate;
 }
 app.get('/', function (req, res) {
