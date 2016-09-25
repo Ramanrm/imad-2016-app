49c39219753c5b7ddd37462bcb2f40083929e0e9
Ramanathan Raman <ramanathangcemech@gmail.com>
2016-09-25 11:36:03 +0530

    js changes

--- ui/main.js
+++ ui/main.js
@@ -2,27 +2,28 @@ var button = document.getElementById('counter');
 
 
 button.onclick = function() {
-button.innerHTML= "click this";
+
 //create a request object
 var request = new XMLHttpRequest();
-alert("request object created");
+
 //capture the response and store in a variable
 request.onreadystatechange = function() {
-alert("entering function");
-	if(request.readyState === XMLHttpRequest.done) {
-alert("inside if 1");
+
+	if(request.readyState === XMLHttpRequest.DONE) {
+
 		if(request.status === 200){
-alert("inside if 2");
+
 			var counter = request.responseText;
 			//render the variable's value in the correct span
-			var span = getElementById('count');
+			
+			var span = document.getElementById('count');
 			span.innerHTML = counter.toString();
 		
 		}
 	}
 };
 //make a request
-request.open('GET','localhost:8080/counter',true);
+request.open('GET','http://localhost:8080/counter',true);
 request.send(null);
 };
 
