0119f7c97e3c5c71943ba55c3f2687aa3f404b32
Ramanrm <ramanathangcemech@gmail.com>
2016-10-25 20:19:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,6 @@
-var button = document.getElementById('counter');
-
-
-button.onclick = function() {
-
-//create a request object
-var request = new XMLHttpRequest();
-
-//capture the response and store in a variable
-request.onreadystatechange = function() {
-
-	if(request.readyState === XMLHttpRequest.DONE) {
-
-		if(request.status === 200){
-
-			var counter = request.responseText;
-			//render the variable's value in the correct span
-			
-			var span = document.getElementById('count');
-			span.innerHTML = counter.toString();
-		
-		}
-	}
-};
-//make a request
-request.open('GET','http://localhost:8080/counter',true);
-request.send(null);
-};
-
-
-
-
+$(document).ready(function() {
+	$(".pill").click(function() {
+		$(".pill").removeClass("active");
+		$(this).addClass("active");
+	});
+});
