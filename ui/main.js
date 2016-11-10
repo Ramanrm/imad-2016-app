4ec4e0cbb5b3c2981e31bbfa4538b757178d70bd
Ramanathan Raman <ramanathangcemech@gmail.com>
2016-11-11 00:23:25 +0530

    adding changes

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
+console.log("Hi. I am main.js.");
 $(document).ready(function() {
-	$(".pill").click(function() {
-		$(".pill").removeClass("active");
-		$(this).addClass("active");
-	});
+    $(".pill").click(function() {
+        $(".pill").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("button").click(function(e){
+    	$("#about").toggleClass("overlay");
+    });
 });
+
